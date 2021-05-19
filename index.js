0eb1ab8f456cb7d6e6f35976211e1e8282fbb011
Oleg <roxvemm2014@gmail.com>
2021-05-18 21:12:15 -0700

    commit tree 7c04ec0fa7211bfd79dcc0600a555613a3c138c1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const input = document.getElementById('text')
+    input.innerHTML = "This is really cool!" 
+     
+}); 
